abf3083dbc7f1c5a56df9b6e5c8e5f51af973022
James Golick <james@giraffesoft.ca>
2008-01-21 13:00:37 -0500

    add the rest of the plugin

--- lib/enum_field.rb
+++ lib/enum_field.rb
@@ -1,1 +1,19 @@
-# EnumField
+module EnumField
+  def self.included(klass)
+    klass.class_eval { extend EnumField::ClassMethods }
+  end
+  
+  module ClassMethods
+    def enum_field(field, possible_values)
+      const_set field.to_s.pluralize.upcase, possible_values unless const_defined?(field.to_s.pluralize.upcase)
+  
+      possible_values.each do |current_value|
+        define_method("#{current_value}?") do
+          self.send(field) == current_value
+        end
+      end
+  
+      validates_inclusion_of field, :in => possible_values, :message => "invalid #{field}"
+    end
+  end
+end
--- test/enum_field_test.rb
+++ test/enum_field_test.rb
@@ -1,8 +1,39 @@
+$LOAD_PATH.reject! { |path| path.include?('TextMate') }
 require 'test/unit'
+require 'rubygems'
+require 'active_support'
+require 'mocha'
+require 'active_record'
+require File.dirname(__FILE__)+'/../lib/enum_field'
+require File.dirname(__FILE__)+'/../init'
+
+class MockedModel; include EnumField; end;
 
 class EnumFieldTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  def setup
+    @possible_values = %w( male female )
+    MockedModel.expects(:validates_inclusion_of).with(:gender, :in => @possible_values, :message => "invalid gender")
+    MockedModel.send(:enum_field, :gender, @possible_values)
+  end
+  
+  def test_should_create_constant_with_possible_values_named_as_pluralized_field
+    assert_equal @possible_values, MockedModel::GENDERS
+  end
+  
+  def test_should_create_query_methods_for_each_enum_type
+    model = MockedModel.new
+    
+    model.stubs(:gender).returns("male")
+    assert model.male?
+    model.stubs(:gender).returns("female")
+    assert !model.male?
+    
+    assert model.female?
+    model.stubs(:gender).returns("male")
+    assert !model.female?
+  end
+  
+  def test_should_extend_active_record_base_with_method
+    assert ActiveRecord::Base.respond_to?(:enum_field)
   end
 end
