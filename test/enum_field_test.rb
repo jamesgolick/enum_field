23ed346af1186c0c3390e4b23fbfb975d8652ae5
Mathieu Martin <webmat@gmail.com>
2009-02-09 14:45:23 -0800

    Straight ports of the tests to Shoulda

--- test/enum_field_test.rb
+++ test/enum_field_test.rb
@@ -3,6 +3,7 @@ require 'test/unit'
 require 'rubygems'
 require 'active_support'
 require 'mocha'
+require 'shoulda'
 require 'active_record'
 require File.dirname(__FILE__)+'/../lib/enum_field'
 require File.dirname(__FILE__)+'/../init'
@@ -10,30 +11,33 @@ require File.dirname(__FILE__)+'/../init'
 class MockedModel; include EnumField; end;
 
 class EnumFieldTest < Test::Unit::TestCase
-  def setup
-    @possible_values = %w( male female )
-    MockedModel.expects(:validates_inclusion_of).with(:gender, :in => @possible_values, :message => "invalid gender")
-    MockedModel.send(:enum_field, :gender, @possible_values)
-  end
+  context "with a simple gender enum" do
+    setup do
+      @possible_values = %w( male female )
+      MockedModel.expects(:validates_inclusion_of).with(:gender, :in => @possible_values, :message => "invalid gender")
+      MockedModel.send(:enum_field, :gender, @possible_values)
+    end
   
-  def test_should_create_constant_with_possible_values_named_as_pluralized_field
-    assert_equal @possible_values, MockedModel::GENDERS
-  end
-  
-  def test_should_create_query_methods_for_each_enum_type
-    model = MockedModel.new
+    should "create constant with possible values named as pluralized field" do
+      assert_equal @possible_values, MockedModel::GENDERS
+    end
     
-    model.stubs(:gender).returns("male")
-    assert model.male?
-    model.stubs(:gender).returns("female")
-    assert !model.male?
+    should "create query methods for each enum type" do
+      model = MockedModel.new
+      
+      model.stubs(:gender).returns("male")
+      assert model.male?
+      model.stubs(:gender).returns("female")
+      assert !model.male?
+      
+      assert model.female?
+      model.stubs(:gender).returns("male")
+      assert !model.female?
+    end
     
-    assert model.female?
-    model.stubs(:gender).returns("male")
-    assert !model.female?
-  end
-  
-  def test_should_extend_active_record_base_with_method
-    assert ActiveRecord::Base.respond_to?(:enum_field)
+    should "extend active record base with method" do
+      assert ActiveRecord::Base.respond_to?(:enum_field)
+    end
   end
 end
+
