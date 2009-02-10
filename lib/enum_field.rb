d4216bae1a6981d18c1d54f16e2a4aec275e46cc
Mathieu Martin <webmat@gmail.com>
2009-02-10 10:50:52 -0800

    Add rdoc to the enum_field method, tweak the readme

--- lib/enum_field.rb
+++ lib/enum_field.rb
@@ -4,6 +4,28 @@ module EnumField
   end
   
   module ClassMethods
+    # enum_field encapsulates a validates_inclusion_of and automatically gives you a 
+    # few more goodies automatically.
+    # 
+    #     class Computer < ActiveRecord:Base
+    #       enum_field :status, ['on', 'off', 'standby', 'sleep', 'out of this world']
+    # 
+    #       # Optionally with a message to replace the default one
+    #       # enum_field :status, ['on', 'off', 'standby', 'sleep'], :message => "incorrect status"
+    # 
+    #       #...
+    #     end
+    # 
+    # This will give you a few things:
+    # 
+    # - add a validates_inclusion_of with a simple error message ("invalid #{field}") or your custom message
+    # - define the following query methods, in the name of expressive code:
+    #   - on?
+    #   - off?
+    #   - standby?
+    #   - sleep?
+    #   - out_of_this_world?
+    # - define the STATUSES constant, which contains the acceptable values
     def enum_field(field, possible_values, options={})
       message = options[:message] || "invalid #{field}"
       const_set field.to_s.pluralize.upcase, possible_values unless const_defined?(field.to_s.pluralize.upcase)
